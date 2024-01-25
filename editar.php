b05adafe6823c34d7abd596920eb180b2a414e00
mario123412345 <mariosv@msmk.university>
2024-01-25 12:07:48 +0100

    cambios en ditar.php

--- editar.php
+++ editar.php
@@ -8,44 +8,37 @@
 <body>
     <?php
     echo "sandia";
-    $mysqli=new mysqli("localhost","root","mario1234@","musica",3306);
+    $mysqli=new mysqli("localhost","root","mario1234@","vehiculos",3306);
     if ($mysqli->connect_errno){
         echo "Fallo (".$mysqli->connect_errno.")".$mysqli->connect_error;
     }
-    if (isset($_GET['nombre'])){
+    if (isset($_GET['id'])){
         if ($_SERVER["REQUEST_METHOD"]=="POST"){
-            $nombre=$_POST['nombre'];
-            $nuevoNombre=$_POST['nuevo_nombre'];
-            $nuevaFechaNacimiento=$_POST['nueva_fecha_nacimiento'];
-            $nuevaFechaMuerte=$_POST['nueva_fecha_muerte'];
-            $nuevoGenero=$_POST['nuevo_genero'];
-            $nuevaHistoria=$_POST['nueva_historia'];
-            $query="UPDATE músico SET 
-                      Nombre_único='$nuevoNombre',
-                      Fecha_nacimiento='$nuevaFechaNacimiento',
-                      Fecha_muerte='$nuevaFechaMuerte',
-                      Nombre_genero='$nuevoGenero',
-                      Historia='$nuevaHistoria'
-                      WHERE Nombre_único='$nombre'";
+            $usuario=$_POST['usuario'];
+            $correo=$_POST['correo'];
+            $contraseña=$_POST['contraseña'];
+            $query="UPDATE personas SET 
+                      usuario='$nuevoNombre',
+                      correo='$nuevocorreo',
+                      contraseña='$nuevacontraseña',
+                      WHERE id='$id'";
             if ($mysqli->query($query)){
-                header("Location:Intermedia3.php");
+                header("Location:tabla.php");
                 exit();
             }else{
                 echo "Error".$mysqli->error;
             }
         }else{
-            $nombre=urldecode($_GET['nombre']);
-            $query="SELECT*FROM músico WHERE Nombre_único='$nombre'";
+            $id=urldecode($_GET['id']);
+            $query="SELECT*FROM personas WHERE id='$id'";
             $result=$mysqli->query($query);
             if ($result->num_rows>0){
                 $row=$result->fetch_assoc();
                 echo "<form action='' method='post'>";
-                echo "<input type='hidden' name='nombre' value='".$row['Nombre_único']."'>";
-                echo "Nombre: <input type='text' name='nuevo_nombre' value='".$row['Nombre_único']."'><br>";
-                echo "Fecha de nacimiento: <input type='text' name='nueva_fecha_nacimiento' value='".$row['Fecha_nacimiento']."'><br>";
-                echo "Fecha de muerte: <input type='text' name='nueva_fecha_muerte' value='".$row['Fecha_muerte']."'><br>";
-                echo "Género: <input type='text' name='nuevo_genero' value='".$row['Nombre_genero']."'><br>";
-                echo "Historia: <input type='text' name='nueva_historia'>".$row['Historia']."</textarea><br>";
+                echo "<input type='hidden' name='usuario' value='".$row['usuario']."'>";
+                echo "Nombre: <input type='text' name='usuario' value='".$row['usuario']."'><br>";
+                echo "Fecha de nacimiento: <input type='text' name='correo' value='".$row['correo']."'><br>";
+                echo "Fecha de muerte: <input type='text' name='contraseña' value='".$row['contraseña']."'><br>";
                 echo "<input type='submit' value='Guardar cambios'>";
                 echo "</form>";
             }else{
@@ -53,7 +46,7 @@
             }
         }
     }else{
-        echo "Escribe el músico";
+        echo "Haz tus cambios";
     }
     echo "huesp";
     ?>
